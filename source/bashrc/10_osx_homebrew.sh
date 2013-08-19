6520426592a00598eae7ad8b6c2f7f497319d8aa
Warpcode <git@warpcode.eu>
2013-08-19 23:25:45 +0100

    Aliases for OSX homebrew

---
+++ source/bashrc/10_osx_homebrew.sh
@@ -0,0 +1,27 @@
+[[ "$OSTYPE" =~ ^darwin ]] || return 1
+
+alias br="brew"
+alias brc="br cleanup"
+alias brpr="br prune"
+alias bri="br install"
+alias brrm="br uninstall"
+alias brls="br list"
+alias brs="br search"
+alias brinf="br info"
+alias brdr="br doctor"
+alias bro="br outdated"
+
+function brcl(){
+    br cleanup
+    br prune
+}
+
+
+function install_homebrew(){
+
+}
+
+
+function uninstall_homebrew(){
+
+}
