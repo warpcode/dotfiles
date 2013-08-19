a7b75478e40802fa09742ecf651df68b8db3083f
Warpcode <git@warpcode.eu>
2013-08-19 22:43:44 +0100

    Add brew update when homebrew is already installed.

--- install/02_osx-homebrew.sh
+++ install/02_osx-homebrew.sh
@@ -6,5 +6,8 @@ e_header "Setting up OSX config"
 if ! require brew; then
     e_process "Homebrew not installed. Installing"
     ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" 2>&1 /dev/null
+else
+    e_process "Updating homebrew install"
+    brew update
 fi
 
