5453eeeb64333dc63e4661b0dee22ff1972fe4c7
Warpcode <git@warpcode.eu>
2013-08-30 22:51:24 +0100

    More brews to install

--- install/03_osx_homebrew.sh
+++ install/03_osx_homebrew.sh
@@ -19,22 +19,77 @@ brew tap homebrew/versions &>/dev/null
 brew tap phinze/homebrew-cask &>/dev/null
 
 
+echo " - Installing brew-cask"
+brew install brew-cask
 
+##
 
+##
+
+#install some core utilities
 echo " - Installing wget"
 brew install wget --enable-iri
 
+echo " - Installing coreutils"
+brew install coreutils
+
 echo " - Installing findutils"
 brew install findutils
-
-echo " - Installing brew-cask"
-brew install brew-cask
 
 echo " - Installing git"
 brew install git
 
 echo " - Installing tree"
 brew install tree
+
+echo " - Installing tmux"
+brew install tmux
+
+echo " - Installing ack"
+brew install ack
+
+echo " - Installing lynx"
+brew install lynx
+
+echo " - Installing nmap"
+brew install nmap
+
+echo " - Installing htop (htop-osx)"
+brew install htop-osx
+
+echo " - Installing man2html"
+brew isntall man2html
+
+
+# Install the lua interpreter
+echo " - Installing lua"
+brew install lua
+
+# JS interpreters
+echo " - Installing ringojs"
+brew install ringojs
+
+echo " - Installing narwhal"
+brew install narwhal
+
+echo " - Installing rhino"
+brew install rhino
+
+echo " - Installing node"
+brew install node
+
+# Screenshot a website
+echo " - Installing webkit2png"
+brew install webkit2png
+
+
+# Compression
+echo " - Installing p7zip"
+brew install p7zip
+
+echo " - Installing zopfli"
+brew install zopfli
+
 #brew cask install "dropbox" &>/dev/null
 
 brew cleanup &>/dev/null
