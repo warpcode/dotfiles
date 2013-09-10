822ac0a27e7e0ed0529a75942e8bea28f047a821
Warpcode <git@warpcode.eu>
2013-09-10 23:12:03 +0100

    cdfinder function to cd to the top most finder window location

--- source/bashrc/10_osx.sh
+++ source/bashrc/10_osx.sh
@@ -7,3 +7,9 @@ if [[ -d "/Applications/Xcode.app" ]]; then
     #iOS Simulator quick launch
     alias iossim="open \"/Applications/Xcode.app/Contents/Applications/iPhone Simulator.app\""
 fi
+
+
+# Change working directory to the top-most Finder window location
+function cdfinder() {
+	cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')"
+}
