828ac738ec62db995d4589dfa5e4431bdcb35458
Warpcode <git@warpcode.eu>
2013-10-16 20:26:11 +0100

    new function to get the first in a list of existing commands

--- source/bashrc/01_misc_func.sh
+++ source/bashrc/01_misc_func.sh
@@ -3,7 +3,17 @@ function _app_exists() {
     hash "${1}" 2>/dev/null || { return 1;}
 }
 
-
+# Grab the first that exists
+# EDITOR=`first_of "mate -w" "nano -w" vi`
+function _app_first_exists() {
+  if [ -n "$1" ]; then
+    local arg=$1
+    shift
+    command -v $arg >> /dev/null && echo $arg || first_of "$@"
+  else
+    exit 1
+  fi
+}
 
 #########################
 # Path Manipulation
