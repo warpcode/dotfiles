f8cf5449585848c379ff0afc23baca0ecc8ca73b
Warpcode <git@warpcode.eu>
2014-02-17 18:54:41 +0000

    move e function to it's own executable file

--- source/bashrc/10_editor.sh
+++ source/bashrc/10_editor.sh
@@ -1,20 +1,3 @@
 
 # Make vim the default editor
 export EDITOR="vim"
-
-#######################
-# Open Editor
-#######################
-function e(){
-    if [[ $EDITOR == '' ]]
-    then
-        echo "\$EDITOR variable is empty"
-        exit 1
-    fi
-
-    if [ "$1" = "" ] ; then
-      exec $EDITOR .
-    else
-      exec $EDITOR "$1"
-    fi
-}
