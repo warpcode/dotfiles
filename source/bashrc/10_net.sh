1be7982d293b7a5ddec2df164f0e91cad32da7be
Warpcode <git@warpcode.eu>
2018-05-02 23:12:14 +0100

    Remove obsolete files

--- source/bashrc/10_net.sh
+++
@@ -1,6 +0,0 @@
-
-# Improved WHOIS lookups
-alias whois="whois -h whois-servers.net"
-
-# OSX flush dns cache
-[[ "$OSTYPE" =~ ^darwin ]] && alias dnsflush="dscacheutil -flushcache && killall -HUP mDNSResponder"
