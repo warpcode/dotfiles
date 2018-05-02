b8b0f97493ed3756009d6180887710ca2ccf042b
Warpcode <git@warpcode.eu>
2018-05-02 22:57:58 +0100

    Remove obsolete

--- source/bashrc/01_misc_func.sh
+++ source/bashrc/01_misc_func.sh
@@ -1,5 +1,5 @@
 # Simple check on whether a binary is accessible via the $PATH
-function _app_exists() { 
+function _app_exists() {
     hash "${1}" 2>/dev/null || { return 1;}
 }
 
@@ -13,28 +13,4 @@ function _app_first_exists() {
   else
     exit 1
   fi
-}
-
-#########################
-# Path Manipulation
-#########################
-
-# Append to $PATH
-function _append_to_path(){
-    _remove_from_path "$@"
-    export PATH="$PATH:$@"
-}
-
-# Prepend to path
-function _prepend_to_path(){
-    _remove_from_path "$@"
-    export PATH="$@:$PATH"
-}
-
-# Remove from $PATH
-function _remove_from_path(){
-    #escape the string first
-    dir=$(echo "$@" | sed -e 's/[\/&]/\\&/g')
-    #remove the path
-    export PATH=$(echo $PATH | sed "s/^${dir}://g" | sed "s/:${dir}$//g" | sed "s/:${dir}:/:/g")
 }
--- source/bashrc/10_net.sh
+++ source/bashrc/10_net.sh
@@ -1,39 +1,6 @@
-# IP info
 
 # Improved WHOIS lookups
 alias whois="whois -h whois-servers.net"
 
 # OSX flush dns cache
 [[ "$OSTYPE" =~ ^darwin ]] && alias dnsflush="dscacheutil -flushcache && killall -HUP mDNSResponder"
-
-# View live HTTP traffic
-alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
-
-# Another traffic sniffer
-alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
-
-# All the dig info
-function digga() {
-	dig +nocmd "$1" any +multiline +noall +answer
-}
-
-#Checks whether a domain could be registered or not
-domainreg () {
-  dig soa $1 | grep -q ^$1 && echo "Registered" || echo "Not Registered"
-}
-
-
-#Port scanner
-portscan () {
-  HOST="$1";
-  for((port=1;port<=65535;++port));do 
-      echo -en "$port ";
-      if echo -en "open $HOST $port\nlogout\quit" | telnet 2>/dev/null | grep 'Connected to' > /dev/null;then 
-          echo -en "\n\nport $port/tcp is open\n\n";
-      fi;
-  done
-}
-
-#Url encoding and decoding
-alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
-alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
