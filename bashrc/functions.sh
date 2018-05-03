74156d2c7ca94f5576bc05d83c3d5236af8dad81
Warpcode <git@warpcode.eu>
2018-05-03 20:51:53 +0100

    Rename function to something more sensible and easy to remmember

--- bashrc/functions.sh
+++ bashrc/functions.sh
@@ -1,6 +1,6 @@
 # Find the most commonly used executables in order
-function ha(){
-    history | awk '{a[$4]++}END{for(i in a){print a[i] " " i}}' | sort -rn | head -n "${@:-20}";
+function history_sort(){
+    history | awk '{a[$4]++}END{for(i in a){print a[i] " " i}}' | sort -rn | head -n "${1:-20}";
 }
 
 # create directory and cd into it
