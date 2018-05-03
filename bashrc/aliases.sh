419e5bd6044f3af0734095d7bd38630672b21d99
Warpcode <git@warpcode.eu>
2018-05-03 20:55:40 +0100

    Add mamp aliases for multiple PHP versions

--- bashrc/aliases.sh
+++ bashrc/aliases.sh
@@ -60,6 +60,15 @@ then
         #iOS Simulator quick launch
         alias iossim="open \"/Applications/Xcode.app/Contents/Applications/iPhone Simulator.app\""
     fi
+
+    #Create aliases for mamp
+    for f in /Applications/MAMP/bin/php/*; do
+        f=`basename "$f"`
+
+        if [[ -f /Applications/MAMP/bin/php/${f}/bin/php ]]; then
+            alias "mamp_${f}"="/Applications/MAMP/bin/php/${f}/bin/php"
+        fi
+    done;
 fi
 
 #######################
