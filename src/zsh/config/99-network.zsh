864ac78b90695436e8899f2b576af4ade9443c5f
Warpcode <warpcodeuk@gmail.com>
2023-03-02 22:48:41 +0000

    Additional helpers and moving more scripts

--- src/zsh/config/99-network.zsh
+++ src/zsh/config/99-network.zsh
@@ -83,3 +83,56 @@ traffic_sniff() {
         sudo ngrep -d "$1" -t '^(GET|POST) ' 'tcp and port 80'
     fi
 }
+
+
+# Downloads all the contents of an open directory
+opendirdl(){
+    if [ -z "$1" ]; then
+        # display usage if no parameters given
+        echo "Usage: opendirdl <link>"
+    else
+        wget -m -e robots=off --no-parent --reject="index.html*" $@
+    fi
+}
+
+# Exposes a directory as a HTTP open directory
+opendir_server() {
+    port="${1:-8000}";
+    # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
+    # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
+    python3 -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port";
+}
+
+# Show all the names (CNs and SANs) listed in the SSL certificate
+# for a given domain
+function getcertnames() {
+	if [ -z "${1}" ]; then
+		echo "ERROR: No domain specified.";
+		return 1;
+	fi;
+
+	local domain="${1}";
+	echo "Testing ${domain}…";
+	echo ""; # newline
+
+	local tmp=$(echo -e "GET / HTTP/1.0\nEOT" \
+		| openssl s_client -connect "${domain}:443" -servername "${domain}" 2>&1);
+
+	if [[ "${tmp}" = *"-----BEGIN CERTIFICATE-----"* ]]; then
+		local certText=$(echo "${tmp}" \
+			| openssl x509 -text -certopt "no_aux, no_header, no_issuer, no_pubkey, \
+			no_serial, no_sigdump, no_signame, no_validity, no_version");
+		echo "Common Name:";
+		echo ""; # newline
+		echo "${certText}" | grep "Subject:" | sed -e "s/^.*CN=//" | sed -e "s/\/emailAddress=.*//";
+		echo ""; # newline
+		echo "Subject Alternative Name(s):";
+		echo ""; # newline
+		echo "${certText}" | grep -A 1 "Subject Alternative Name:" \
+			| sed -e "2s/DNS://g" -e "s/ //g" | tr "," "\n" | tail -n +2;
+		return 0;
+	else
+		echo "ERROR: Certificate not found.";
+		return 1;
+	fi;
+}
