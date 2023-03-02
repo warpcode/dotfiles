a30428cddffa1e7815c4634ec74a338435b040d4
Warpcode <warpcodeuk@gmail.com>
2023-03-02 22:27:35 +0000

    Moving more bin files to scripts

--- src/zsh/config/99-filesystem.zsh
+++ src/zsh/config/99-filesystem.zsh
@@ -42,3 +42,16 @@ fi
 alias desktop="cd ~/Desktop"
 alias downloads="cd ~/Downloads"
 alias dotf="cd '$DOTFILES'"
+
+fs() {
+    if du -b /dev/null > /dev/null 2>&1; then
+        arg=-sbh;
+    else
+        arg=-sh;
+    fi
+    if [[ -n "$@" ]]; then
+        du $arg -- "$@";
+    else
+        du $arg .[^.]* ./*;
+    fi;
+}
--- src/zsh/config/99-network.zsh
+++ src/zsh/config/99-network.zsh
@@ -36,3 +36,50 @@ dataurl() {
         echo "data:${MIMETYPE};base64,$(openssl base64 -in "$1" | tr -d '\n')";
     fi
 }
+
+# Check whether a domain is registered or not
+domain_is_registered() {
+    if [ -z "$1" ]; then
+        # display usage if no parameters given
+        echo "Usage: domain_registered <domain>"
+    else
+        dig soa $1 | grep -q ^$1 && echo "Registered" || echo "Not Registered"
+    fi
+}
+
+# View live HTTP traffic
+httpdump() {
+    if [ -z "$1" ]; then
+        # display usage if no parameters given
+        echo "Usage: httpdump <network_interface>"
+    else
+        sudo tcpdump -i "$1" -n -s 0 -w - | grep -a -o -E "Host\: .*|GET \/.*"
+    fi
+}
+
+# Post scans a single host
+# port_scan() {
+#     if [ -z "$1" ]; then
+#         # display usage if no parameters given
+#         echo "Usage: port_scan <host>"
+#     else
+#         HOST="$1";
+#         for((port=1;port<=65535;++port));do
+#             if echo -en "open $HOST $port\nlogout\quit" | telnet 2>/dev/null | grep 'Connected to' > /dev/null;then
+#                 echo -en "port $port/tcp is open\n";
+#             fi;
+#         done
+#     fi
+# }
+
+# Another traffic sniffer
+traffic_sniff() {
+    hash ngrep 2>/dev/null || { echo 'Please install ngrep'; exit;}
+
+    if [ -z "$1" ]; then
+        # display usage if no parameters given
+        echo "Usage: sniff <network_interface>"
+    else
+        sudo ngrep -d "$1" -t '^(GET|POST) ' 'tcp and port 80'
+    fi
+}
