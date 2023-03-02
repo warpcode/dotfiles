136fec6d282098dcd2b5da8e32b7c27b94287c72
Warpcode <warpcodeuk@gmail.com>
2023-03-02 21:37:04 +0000

    Add npm dependencies

--- install.zsh
+++ install.zsh
@@ -1,8 +1,19 @@
 #!/usr/bin/env zsh
 set -e
 
+pushd ${0:A:h}
+
 # Ensure dependencies are up to date
 git submodule update --init
 
+# Ensure npm is installed and run
+if (( $+commands[npm] )); then
+    [ -f package-lock.json ] && npm ci || npm i
+else
+    popd
+    echo "npm is not installed"
+fi
+
 # Use stow to install dotfiles
+echo "Installing dotfile"
 stow -R --no-folding -t ~/ stow
--- src/zsh/config/01-paths.zsh
+++ src/zsh/config/01-paths.zsh
@@ -30,3 +30,7 @@ path_prepend "/usr/local/bin"
 path_prepend "/usr/local/sbin"
 path_prepend "${HOME}/.local/bin"
 path_prepend "${HOME}/bin"
+
+# Dotfiles bin files should be the least priority
+path_append "${DOTFILES}/bin"
+path_append "${DOTFILES}/node_modules/.bin"
