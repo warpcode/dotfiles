98b16b395ffeb00876eec8700b33ed037c8092db
Warpcode <warpcodeuk@gmail.com>
2023-03-03 21:14:38 +0000

    Set history date format

--- src/zsh/config/01-history.zsh
+++ src/zsh/config/01-history.zsh
@@ -14,6 +14,14 @@ HISTSIZE=1000
 SAVEHIST=1000
 HISTFILE=~/.zsh_history
 
+# Uncomment the following line if you want to change the command execution time
+# stamp shown in the history command output.
+# You can set one of the optional three formats:
+# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+# or set a custom format using the strftime function format specifications,
+# see 'man strftime' for details.
+HIST_STAMPS="dd/mm/yyyy"
+
 # Allow large history
 export HISTFILESIZE=10000
 
--- src/zsh/config/50-oh-my-zsh.zsh
+++ src/zsh/config/50-oh-my-zsh.zsh
@@ -54,14 +54,6 @@ ZSH_THEME="robbyrussell"
 # much, much faster.
 # DISABLE_UNTRACKED_FILES_DIRTY="true"
 
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
-# You can set one of the optional three formats:
-# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# or set a custom format using the strftime function format specifications,
-# see 'man strftime' for details.
-# HIST_STAMPS="mm/dd/yyyy"
-
 # Would you like to use another custom folder than $ZSH/custom?
 # ZSH_CUSTOM="$(dirname ${0:A:h})/ohmyzsh"
 
