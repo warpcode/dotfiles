5b449bb97c54e2ec199d3c10ada63dcf28d5fefc
Warpcode <warpcodeuk@gmail.com>
2023-03-06 20:35:29 +0000

    Add zsh autosuggestions and powerlevel10 theme

--- src/zsh/config/50-oh-my-zsh.zsh
+++ src/zsh/config/50-oh-my-zsh.zsh
@@ -8,7 +8,7 @@ export ZSH="$DOTFILES/src/zsh/ohmyzsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="robbyrussell"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -66,6 +66,7 @@ plugins=(
     aliases
     git
     gitignore
+    zsh-autosuggestions
 )
 
 source $ZSH/oh-my-zsh.sh
