48c40614291fd1ac4711c556e264fb8a92404f8f
Warpcode <warpcodeuk@gmail.com>
2023-03-16 22:54:56 +0000

    Fix dircolors where it might not be available

--- src/zsh/config/01-defaults.zsh
+++ src/zsh/config/01-defaults.zsh
@@ -17,7 +17,6 @@ zstyle ':completion:*' completer _expand _complete _correct _approximate
 zstyle ':completion:*' format 'Completing %d'
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' menu select=2
-eval "$(dircolors -b)"
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
@@ -30,6 +29,9 @@ zstyle ':completion:*' verbose true
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
 zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
 
+if (( $+commands[dircolors] )); then
+    eval "$(dircolors -b)"
+fi
 
 # Don’t clear the screen after quitting a manual page
 export MANPAGER="less -X"
