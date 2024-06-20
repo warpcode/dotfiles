659b57ad7e019efe029c002f6ce29d74060d3ec8
Warpcode <warpcodeuk@gmail.com>
2024-06-20 16:56:25 +0100

    Increase history limits

--- src/zsh/config/02-history.zsh
+++ src/zsh/config/02-history.zsh
@@ -8,10 +8,11 @@ setopt sharehistory
 setopt INC_APPEND_HISTORY
 setopt HIST_FIND_NO_DUPS
 setopt HIST_IGNORE_SPACE
+setopt appendhistory
 
 # Keep 1000 lines of history within the shell and save it to ~/.zsh_history:
-HISTSIZE=1000
-SAVEHIST=1000
+HISTSIZE=10000
+SAVEHIST=10000
 HISTFILE=~/.zsh_history
 
 # Uncomment the following line if you want to change the command execution time
