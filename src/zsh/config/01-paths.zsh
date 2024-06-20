e703fe2ed5f8171c8aba649785941857757aca3b
Warpcode <warpcodeuk@gmail.com>
2024-06-20 17:00:30 +0100

    Remove bin paths that no longer exist

--- src/zsh/config/01-paths.zsh
+++ src/zsh/config/01-paths.zsh
@@ -5,5 +5,3 @@ path_prepend "${HOME}/bin"
 
 # Dotfiles bin files should be the least priority
 path_append "${DOTFILES}/bin"
-path_append "${DOTFILES}/node_modules/.bin"
-path_append "${DOTFILES}/php_modules/bin"
