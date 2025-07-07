7d796a5378b06cbb6af1e5a25c4fc3892f4be53d
Warpcode <warpcodeuk@gmail.com>
2025-07-07 15:51:19 +0100

    Remove things i no longer use

--- src/zsh/config/70-filesystem.zsh
+++ src/zsh/config/70-filesystem.zsh
@@ -26,39 +26,12 @@ function mkd() {
     mkdir -p "$@" && cd "$@"
 }
 
-if [[ "$OSTYPE" =~ ^darwin ]]
-then
-    # Change working directory to the top-most Finder window location
-    function cdfinder() {
-        cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')"
-    }
-fi
 
 #######################
 # Shortcuts
 #######################
 alias desktop="cd ~/Desktop"
 alias downloads="cd ~/Downloads"
-alias dotf="cd '$DOTFILES'"
-
-fs() {
-    if du -b /dev/null > /dev/null 2>&1; then
-        arg=-sbh;
-    else
-        arg=-sh;
-    fi
-    if [[ -n "$@" ]]; then
-        du $arg -- "$@";
-    else
-        du $arg .[^.]* ./*;
-    fi;
-}
-
-
-# `tre` is a shorthand for `tree` with hidden files and color enabled, ignoring
-# the `.git` directory, listing directories first. The output gets piped into
-# `less` with options to preserve color and line numbers, unless the output is
-# small enough for one screen.
-function tre() {
-	tree -aC -I '.git|node_modules|bower_components' --dirsfirst "$@" | less -FRNX;
-}
+alias docs="cd ~/Documents"
+alias dotfiles="cd '$DOTFILES'"
+alias nvim.conf="git_clone_and_go 'git@github.com:warpcode/vim-config.git' ~/src/vim-config/"
---
+++ src/zsh/functions/aliases.zsh
@@ -0,0 +1,23 @@
+function git_clone_and_go() {
+    if [[ -z "$1" ]]; then
+        echo "Please provide a valid git url"
+        return 1
+    fi
+
+    ## check if the second parameter is a directory
+    if [[ -z "$2" ]]; then
+        local dir_name=$(basename "$1" .git)
+    else
+        local dir_name="$2"
+    fi
+
+    # If the destination dir doesn't exist, clone the repo
+    if [[ ! -d "$dir_name" ]]; then
+        git clone --depth 1 --recurse-submodules "$1" "$dir_name"
+    else
+        echo "Directory '$dir_name' already exists. Skipping clone."
+    fi
+
+    # cd to the directory and then open in nvim
+    cd "$dir_name" || { echo "Failed to change directory to '$dir_name'"; return 1; }
+}
