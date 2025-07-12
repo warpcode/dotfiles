e1ee1a3c43c2fa144484fc047928bd305ae29efd
Warpcode <warpcodeuk@gmail.com>
2025-07-12 15:27:48 +0100

    WIP for setting up a tmux session for working on the neovim config

--- src/zsh/apps/neovim.zsh
+++ src/zsh/apps/neovim.zsh
@@ -3,3 +3,16 @@ export NVIM_CONFIG_PATH=~/src/vim-config/
 
 alias nvim.conf="git_clone_and_cd $NVIM_CONFIG_GIT_URL $NVIM_CONFIG_PATH"
 alias nvim.edit="nvim.conf; e .; cd -"
+
+function _nvim_tmux_setup() {
+    nvim.conf
+    tmux att -t neovim ||
+        tmux \
+        new -s neovim -n editor \; \
+        send-keys 'e .' C-m\; \
+        \
+        neww -n git \; \
+        send-keys 'lazygit' C-m\; \
+        \
+        selectw -t editor
+}
