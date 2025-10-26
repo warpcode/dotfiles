78c1136930c28c137301493e40860339ddfbda54
Warpcode <warpcodeuk@gmail.com>
2025-10-26 12:33:53 +0000

    Add Docker Compose management aliases for Copycreator project to streamline development workflow

--- src/zsh/projects/copycreator.zsh
+++ src/zsh/projects/copycreator.zsh
@@ -5,4 +5,12 @@ if [[ ! -d "$COPYCREATOR_DIR" ]]; then
   return
 fi
 
-alias cc.cd="cd \"$COPYCREATOR_DIR\""
+# Copycreator management aliases
+alias cc.db="pushd \"$COPYCREATOR_DIR\" > /dev/null && docker compose exec web copycreator-db.sh; popd > /dev/null"
+alias cc.sh="pushd \"$COPYCREATOR_DIR\" > /dev/null && docker compose exec web bash; popd > /dev/null"
+
+# Docker management aliases
+alias cc.up="pushd \"$COPYCREATOR_DIR\" > /dev/null && docker compose up -d --build; popd > /dev/null"
+alias cc.stop="pushd \"$COPYCREATOR_DIR\" > /dev/null && docker compose stop; popd > /dev/null"
+alias cc.restart="cc.stop && cc.up"
+alias cc.logs="pushd \"$COPYCREATOR_DIR\" > /dev/null && docker compose logs -f --tail=100 web; popd > /dev/null"
