0070a175bd5f6db8a5d86b13a6b3fddfadff62d7
Warpcode <warpcodeuk@gmail.com>
2025-10-27 13:55:18 +0000

    Use subshells to prevent cwd changing

--- src/zsh/projects/m6.zsh
+++ src/zsh/projects/m6.zsh
@@ -9,23 +9,23 @@ fi
 alias m6.cd="cd \"$MARTINI_DIR\""
 
 # Martini management aliases
-alias m6.sh="pushd \"$MARTINI_DIR\" > /dev/null && docker compose exec web bash; popd > /dev/null"
+alias m6.sh="(cd \"$MARTINI_DIR\" && docker compose exec web bash)"
 
 # Database management
-alias m6.db="pushd \"$MARTINI_DIR\" > /dev/null && docker compose exec web martini-db.sh; popd > /dev/null"
+alias m6.db="(cd \"$MARTINI_DIR\" && docker compose exec web martini-db.sh)"
 alias m6.db.dump="pushd \"$MARTINI_DIR\" > /dev/null && docker compose exec mysql mysqldump -u\$(_m6_get_config_json martini-db '.username') -p\$(_m6_get_config_json martini-db '.password') \$(_m6_get_config_json martini-db '.name') > backup_\$(date +%Y%m%d_%H%M%S).sql; popd > /dev/null"
-alias m6.db.query="pushd \"$MARTINI_DIR\" > /dev/null && docker compose exec web martini-db.sh -e"
-alias m6.db.tables="pushd \"$MARTINI_DIR\" > /dev/null && docker compose exec web martini-db.sh -e 'SHOW TABLES;'; popd > /dev/null"
+alias m6.db.query="(cd \"$MARTINI_DIR\" && docker compose exec web martini-db.sh -e"
+alias m6.db.tables="(cd \"$MARTINI_DIR\" && docker compose exec web martini-db.sh -e 'SHOW TABLES;')"
 
 # Docker management aliases
-alias m6.up="pushd \"$MARTINI_DIR\" > /dev/null && docker compose up -d --build; popd > /dev/null"
-alias m6.stop="pushd \"$MARTINI_DIR\" > /dev/null && docker compose stop; popd > /dev/null"
+alias m6.up="(cd \"$MARTINI_DIR\" && docker compose up -d --build)"
+alias m6.stop="(cd \"$MARTINI_DIR\" && docker compose stop)"
 alias m6.restart="m6.stop && m6.up"
-alias m6.logs="pushd \"$MARTINI_DIR\" > /dev/null && docker compose logs -f --tail=100 web; popd > /dev/null"
+alias m6.logs="(cd \"$MARTINI_DIR\" && docker compose logs -f --tail=100 web)"
 
 # FE tools
-alias m6.build.mds="pushd \"$MARTINI_DIR\"/misc/responsive-static/mds > /dev/null && npm run build; popd > /dev/null"
-alias m6.build.admin="pushd \"$MARTINI_DIR\"/misc/responsive-static/admin > /dev/null && npm run build; popd > /dev/null"
+alias m6.build.mds="(cd \"$MARTINI_DIR\"/misc/responsive-static/mds && npm run build)"
+alias m6.build.admin="(cd \"$MARTINI_DIR\"/misc/responsive-static/admin && npm run build)"
 alias m6.build.all="m6.build.mds && m6.build.admin"
 
 # Staging aliases
@@ -43,11 +43,9 @@ _m6_get_config_json() {
         return 1
     fi
 
-    pushd "$MARTINI_DIR" > /dev/null || { echo "Error: martini directory not found" >&2; return 2; }
-    # Only output JSON, suppress all other output
-    local json_output=$(php -r 'chdir("etc"); $key = $argv[1];require "martini-config-docker.php";$value = Config::get($key);echo json_encode($value);' "$key" 2>/dev/null)
+    # Run in subshell to avoid changing parent directory
+    local json_output=$(cd "$MARTINI_DIR" && php -r 'chdir("etc"); $key = $argv[1];require "martini-config-docker.php";$value = Config::get($key);echo json_encode($value);' "$key" 2>/dev/null)
     local ret=$?
-    popd > /dev/null
 
     if [[ $ret -eq 0 ]]; then
         if [[ -n "$jq_filter" ]]; then
