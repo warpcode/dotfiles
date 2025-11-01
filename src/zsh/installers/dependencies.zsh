f45a368865a0e3a889bf7c3c915ca64e19583fec
Warpcode <warpcodeuk@gmail.com>
2025-11-01 15:14:10 +0000

    Ensure jq is installed

--- src/zsh/installers/dependencies.zsh
+++ src/zsh/installers/dependencies.zsh
@@ -1,7 +1,7 @@
 # Common dependencies required for repository setup and package installation
 
 # Debian/Ubuntu prerequisites for adding repositories and installing packages
-_installer_dependencies "debian" ca-certificates curl gnupg
+_installer_dependencies "debian" ca-certificates curl gnupg jq
 
 # Fedora prerequisites for package management
 _installer_dependencies "fedora" dnf-plugins-core
