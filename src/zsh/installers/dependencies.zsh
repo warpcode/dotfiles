ac047fcb1a12fb21969550b1790f92cbdb065290
Warpcode <warpcodeuk@gmail.com>
2025-11-01 14:44:38 +0000

    Remove test

--- src/zsh/installers/dependencies.zsh
+++ src/zsh/installers/dependencies.zsh
@@ -2,7 +2,6 @@
 
 # Debian/Ubuntu prerequisites for adding repositories and installing packages
 _installer_dependencies "debian" ca-certificates curl gnupg
-_installer_dependencies "debian" gnupg
 
 # Fedora prerequisites for package management
 _installer_dependencies "fedora" dnf-plugins-core
