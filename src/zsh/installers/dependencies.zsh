0066ef4593f51f1f731cf56b1e5e0ca518b4bb16
Warpcode <warpcodeuk@gmail.com>
2025-11-02 12:48:37 +0000

    Add unzip dependency for compression tool support

--- src/zsh/installers/dependencies.zsh
+++ src/zsh/installers/dependencies.zsh
@@ -1,7 +1,13 @@
 # Common dependencies required for repository setup and package installation
 
 # Debian/Ubuntu prerequisites for adding repositories and installing packages
-_installer_dependencies "debian" ca-certificates curl gnupg jq
+_installer_dependencies "debian" ca-certificates curl gnupg jq unzip
 
 # Fedora prerequisites for package management
-_installer_dependencies "fedora" dnf-plugins-core
+_installer_dependencies "fedora" dnf-plugins-core unzip
+
+# Arch Linux prerequisites
+_installer_dependencies "arch" unzip
+
+# macOS prerequisites (via Homebrew)
+_installer_dependencies "macos" unzip
