07762be9b16ad9e7b23dc20009634a0eaf75d6f6
Warpcode <warpcodeuk@gmail.com>
2025-12-17 18:11:40 +0000

    feat(zsh/homelab-docker): add hd.env.gen function to generate .env with API keys

--- src/zsh/projects/homelab-docker.zsh
+++ src/zsh/projects/homelab-docker.zsh
@@ -9,3 +9,20 @@ fi
 # Navigation
 alias hd.cd="_git_clone_and_cd \"$HOMELAB_DOCKER_REPO\" \"$HOMELAB_DOCKER_DIR\""
 
+hd.env.gen() {
+    (
+        set -e
+        hd.cd
+        kp.login
+        {
+            _env_kv "DOCKER_CONTEXT" "remote-docker"
+            _env_kv "BASE_HOSTNAME" "warpcode.co.uk"
+            _env_kv "LITELLM_MASTER_KEY" "sk-1234"
+            _env_kv "GEMINI_API_KEY" "$(kp show 'websites/email/google.main' -a gemini_api_key)"
+            _env_kv "GITHUB_API_KEY" "$(kp show 'KeePassXC-Browser Passwords/Github' -a api_key_docker_ai)"
+            _env_kv "GROQ_API_KEY" "$(kp show 'KeePassXC-Browser Passwords/Groq' -a api_key_docker)"
+            _env_kv "OPENAI_API_KEY" "$(kp show 'KeePassXC-Browser Passwords/OpenRouter' -a api_key_docker)"
+            _env_kv "OPENROUTER_API_KEY" "$(kp show 'KeePassXC-Browser Passwords/ChatGPT' -a api_key_docker_ai)"
+        }  > "$HOMELAB_DOCKER_DIR/.env"
+    )
+}
