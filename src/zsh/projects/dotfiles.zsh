1b808e39bbbfb7d132b743f7f95cda6aa16c7fe9
Warpcode <warpcodeuk@gmail.com>
2026-01-10 20:55:37 +0000

    chore(zsh): remove deprecated packages.zsh system
    - Delete obsolete packages.zsh function file
    
    - Remove df.install() function that relied on old system
    
    - Clean up legacy package registration code

--- src/zsh/projects/dotfiles.zsh
+++ src/zsh/projects/dotfiles.zsh
@@ -1,19 +1,6 @@
 alias df.cd="cd '$DOTFILES'"
 alias df.edit="_dotfiles_tmux_setup"
 
-function df.install() {
-    local os=$(_os_detect_os_family)
-    _events_trigger "installer_pre_deps" "$os"
-    _packages_install_apps $(_packages_get_apps_by_tag dep)
-    _packages_add_registered_keys
-    _packages_add_registered_repos
-    _events_trigger "installer_post_deps" "$os"
-    _events_trigger "installer_pre_install" "$os"
-    _packages_install_apps $(_packages_get_apps_by_tag default)
-    _events_trigger "installer_post_install"
-    echo "🎉 Installation complete"
-}
-
 function _dotfiles_tmux_setup() {
     df.cd
     _tmux_basic_git "dotfiles"
