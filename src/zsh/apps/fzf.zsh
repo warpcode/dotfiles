1987dbd578d5da1c0d3984cc84d1a87ce962496b
Warpcode <warpcodeuk@gmail.com>
2026-05-23 18:27:24 +0100

    refactor: migrate oh-my-zsh from git submodule to chezmoi external and remove unused ai-skills recipe

--- src/zsh/apps/fzf.zsh
+++ src/zsh/apps/fzf.zsh
@@ -8,6 +8,6 @@ if [[ -n "$fzf_init" ]]; then
     return 0
 fi
 
-local fzf_plugin="$DOTFILES/vendor/ohmyzsh/plugins/fzf/fzf.plugin.zsh"
+local fzf_plugin="$HOME/.local/opt/ohmyzsh/plugins/fzf/fzf.plugin.zsh"
 [[ -f "$fzf_plugin" ]] || return 1
 source "$fzf_plugin"
--- src/zsh/config/50-oh-my-zsh.zsh
+++ src/zsh/config/50-oh-my-zsh.zsh
@@ -5,7 +5,7 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="${ZSH:-$DOTFILES/vendor/ohmyzsh}"
+export ZSH="${ZSH:-$HOME/.local/opt/ohmyzsh}"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
