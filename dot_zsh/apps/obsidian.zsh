27c990928492285fe32cc1897e0a66dc7e85dd31
Warpcode <warpcodeuk@gmail.com>
2026-06-22 17:20:45 +0100

    refactor: remove legacy zsh application wrappers

--- dot_zsh/apps/obsidian.zsh
+++
@@ -1,7 +0,0 @@
-# @description Obsidian aliases and helper functions.
-# Extracted from previous implementations, now wrapping bin/df.obsidian
-
-alias onn='df.obsidian new'
-alias ofa='df.obsidian find-by-attr'
-alias oav='df.obsidian values'
-
--- dot_zsh/apps/pkg/recipes/dev/vcs/git.zsh
+++
@@ -1,3 +0,0 @@
-pkg.recipe.define git \
-    package="git" \
-    managers="apt dnf pacman brew"
